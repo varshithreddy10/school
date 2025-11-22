3e49fef256c59d7423c2768b583d54251771b7cf
varshith reddy <varshithreddy1009@gmail.com>
2025-11-23 02:11:23 +0530

    Updated application.properties and fixed DB config

--- src/main/java/com/students/students/controller/StudentController.java
+++ src/main/java/com/students/students/controller/StudentController.java
@@ -23,7 +23,7 @@ public class StudentController {
     @GetMapping("/start")
     public String start()
     {
-        String message = " this is the student management application written by the nallavelli varshith kumar reddy of mtech";
+        String message = " this is the student management application written by the nallavelli varshith kumar reddy ";
         return message;
     }
 
