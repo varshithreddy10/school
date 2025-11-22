740b444bad483f074b53b2eb3a7c3670da78c719
N VARSHITH KUMAR REDDY <varshithreddy1009@gmail.com>
2025-11-23 02:59:45 +0530

    Update StudentController.java

--- src/main/java/com/students/students/controller/StudentController.java
+++ src/main/java/com/students/students/controller/StudentController.java
@@ -23,7 +23,7 @@ public class StudentController {
     @GetMapping("/start")
     public String start()
     {
-        String message = " this is the student management application written by the nallavelli varshith kumar reddy vardhaman college of engineering hyderabad";
+        String message = " hello i am varshith reddy";
         return message;
     }
 
